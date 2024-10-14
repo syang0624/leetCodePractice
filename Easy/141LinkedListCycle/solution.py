f7bb2e8bd99059fbceafd9fc3d4733b5ec5d31dd
syang0624 <steven.yang@uni.minerva.edu>
2024-10-14 16:08:25 -0700

    Problem141 Done

---
+++ Easy/141LinkedListCycle/solution.py
@@ -0,0 +1,23 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
