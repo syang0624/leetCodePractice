1ec72d7647193be6311982f966d9700213494c51
syang0624 <steven.yang@uni.minerva.edu>
2024-10-16 11:32:11 -0700

    rename 424 variable

--- Medium/424LongestRepeatingCharacterReplacement/solution.py
+++ Medium/424LongestRepeatingCharacterReplacement/solution.py
@@ -2,12 +2,12 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
         res = 0
-        l = 0
+        left = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            while (r - l + 1) - max(count.values()) > k:
-                count[s[l]] -= 1
-                l += 1
-            res = max(res, r - l + 1)
+            while (r - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+            res = max(res, r - left + 1)
 
         return res
