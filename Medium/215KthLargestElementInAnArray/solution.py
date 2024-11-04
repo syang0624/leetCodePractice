215cd3c69ebce4ca2d41d0412c2c0d6eb0645a2c
syang0624 <steven.yang@uni.minerva.edu>
2024-11-03 16:43:12 -0800

    problem 215 done

---
+++ Medium/215KthLargestElementInAnArray/solution.py
@@ -0,0 +1,25 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # Max Heap Solution
+        # for i in range(len(nums)):
+        #     nums[i] = -nums[i]
+
+        # heapq.heapify(nums)
+
+        # for _ in range(k-1):
+        #     heapq.heappop(nums)
+
+        # return -heapq.heappop(nums)
+
+        # Min Heap Solution
+        min_heap = []
+        for num in nums:
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, num)
+            else:
+                heapq.heappushpop(min_heap, num)
+        return min_heap[0]
