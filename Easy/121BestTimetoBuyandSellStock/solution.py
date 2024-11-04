853a45909bf2d05bfd506f8aaa51f15a9ca728c1
syang0624 <steven.yang@uni.minerva.edu>
2024-11-04 10:26:06 -0800

    problem 121 more efficient way

--- Easy/121BestTimetoBuyandSellStock/solution.py
+++ Easy/121BestTimetoBuyandSellStock/solution.py
@@ -1,12 +1,25 @@
 from typing import List
 
 
+# class Solution:
+#     # Two pointers
+#     def maxProfit(self, prices: List[int]) -> int:
+#         min_price = prices[0]
+#         max_price = 0
+#         for a in prices[1:]:
+#             max_price = max(max_price, a - min_price)
+#             min_price = min(min_price, a)
+#         return max_price
 class Solution:
-    # Two pointers
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
-        max_price = 0
-        for a in prices[1:]:
-            max_price = max(max_price, a - min_price)
-            min_price = min(min_price, a)
-        return max_price
+        left, right = 0, 1
+        maxProfit = 0
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxProfit = max(maxProfit, profit)
+            else:
+                left = right
+            right += 1
+
+        return maxProfit
