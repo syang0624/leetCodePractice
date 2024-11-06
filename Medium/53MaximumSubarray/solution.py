5c5541d5328bf5d6d1d9e279f2963fac005266bd
syang0624 <steven.yang@uni.minerva.edu>
2024-11-06 10:50:26 -0800

    problem 53 tried again

--- Medium/53MaximumSubarray/solution.py
+++ Medium/53MaximumSubarray/solution.py
@@ -6,11 +6,19 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        globalMax = nums[0]
-        localMax = nums[0]
-        
-        for i in range(1, len(nums)):
-            localMax = max(nums[i], localMax + nums[i])
-            globalMax = max(globalMax, localMax)
-            
-        return globalMax
+        # globalMax = nums[0]
+        # localMax = nums[0]
+
+        # for i in range(1, len(nums)):
+        #     localMax = max(nums[i], localMax + nums[i])
+        #     globalMax = max(globalMax, localMax)
+
+        # return globalMax
+        maxSum = nums[0]
+        currentSum = 0
+        for n in nums:
+            currentSum += n
+            maxSum = max(maxSum, currentSum)
+            if currentSum < 0:
+                currentSum = 0
+        return maxSum
