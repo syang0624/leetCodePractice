be9b5047357234f5c6429a5606438ab0ad9a091d
syang0624 <steven.yang@uni.minerva.edu>
2024-11-07 14:17:08 -0800

    problem 1 hashmap solution update

--- Easy/1TwoSum/solution.py
+++ Easy/1TwoSum/solution.py
@@ -1,11 +1,24 @@
 from typing import List
-# Time O(N^2)
-# Space O(1)
 
 
 class Solution:
+    # Brute Force
+    # Time O(N^2)
+    # Space O(1)
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for x in range(len(nums)):
+    #         for y in range(x+1, len(nums)):
+    #             if nums[x] + nums[y] == target:
+    #                 return [x, y]
+
+    # HashMap
+    # Time O(N)
+    # Space O(N)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
         for x in range(len(nums)):
-            for y in range(x+1, len(nums)):
-                if nums[x] + nums[y] == target:
-                    return [x, y]
+            hashMap[nums[x]] = x
+        for i in range(len(nums)):
+            y = target - nums[i]
+            if y in hashMap and hashMap[y] != i:
+                return [i, hashMap[y]]
