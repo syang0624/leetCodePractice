b6b2a59b5f79f5ce53a53fa293197178548e5915
syang0624 <steven.yang@uni.minerva.edu>
2024-11-17 16:53:23 -0800

    problem 841 shorter code

--- Medium/841KeysAndRooms/solution.py
+++ Medium/841KeysAndRooms/solution.py
@@ -1,20 +1,35 @@
 from typing import List
 
 
+# class Solution:
+#     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+#         visited = []
+#         stack = []
+#         visited.append(0)
+#         for x in rooms[0]:
+#             stack.append(x)
+#         while stack:
+#             s = stack.pop()
+#             if s not in visited:
+#                 visited.append(s)
+#                 for x in rooms[s]:
+#                     stack.append(x)
+#         if len(visited) == len(rooms):
+#             return True
+#         else:
+#             return False
+
+
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visited = []
-        stack = []
-        visited.append(0)
-        for x in rooms[0]:
-            stack.append(x)
-        while stack:
-            s = stack.pop()
-            if s not in visited:
-                visited.append(s)
-                for x in rooms[s]:
-                    stack.append(x)
-        if len(visited) == len(rooms):
-            return True
-        else:
-            return False
+        visited = set()
+
+        def dfs(room):
+            visited.add(room)
+            for i in rooms[room]:
+                if i not in visited:
+                    dfs(i)
+
+        dfs(0)
+
+        return len(visited) == len(rooms)
