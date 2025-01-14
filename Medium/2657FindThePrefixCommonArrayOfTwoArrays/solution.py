3c895ca6b44bd2e421e6697e526d853fb225797a
syang0624 <steven.yang@uni.minerva.edu>
2025-01-14 22:04:25 +0900

    problem 2657 done

---
+++ Medium/2657FindThePrefixCommonArrayOfTwoArrays/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        n = len(A)
+        freq = [0] * (n+1)
+        res = [0] * n
+        count = 0
+
+        for i in range(n):
+            freq[A[i]] += 1
+            if freq[A[i]] == 2:
+                count += 1
+            freq[B[i]] += 1
+            if freq[B[i]] == 2:
+                count += 1
+            
+            res[i] = count
+        
+        return res
+    
+# Time: O(N)
+# Space: O(N)
