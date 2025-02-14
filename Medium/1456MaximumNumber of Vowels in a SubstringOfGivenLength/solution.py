a784b3c692335be958b5bd63252b07fc069796aa
syang0624 <steven.yang@uni.minerva.edu>
2025-02-14 17:47:30 +0900

    problem 1456 clean code update

--- Medium/1456MaximumNumber of Vowels in a SubstringOfGivenLength/solution.py
+++ Medium/1456MaximumNumber of Vowels in a SubstringOfGivenLength/solution.py
@@ -1,25 +1,13 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        # TLE solution
-        # maxCount = 0
-        # vowels = [ 'a', 'e', 'i', 'o', 'u']
-        # for i in range(0, len(s)-k+1):
-        #     lst = s[i:i+k]
-        #     localCount = 0
-        #     for j in range(len(lst)):
-        #         if lst[j] in vowels:
-        #             localCount += 1
-        #     maxCount = max(maxCount, localCount)
-        #     if maxCount == k:
-        #         break
-        # return maxCount
-        vowels = ["a", "e", "i", "o", "u"]
-        maxCount, localCount, left = 0, 0, 0
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        left, maxCount, count = 0, 0, 0
         for right in range(len(s)):
-            localCount += 1 if s[right] in vowels else 0
+            if s[right] in vowels:
+                count += 1
             if right - left + 1 > k:
-                localCount -= 1 if s[left] in vowels else 0
+                if s[left] in vowels:
+                    count -= 1
                 left += 1
-            maxCount = max(maxCount, localCount)
-
+            maxCount = max(maxCount, count)
         return maxCount
