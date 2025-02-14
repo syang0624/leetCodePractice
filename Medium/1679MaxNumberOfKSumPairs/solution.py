cca3824143a52ee2603566d5feab4b46f1a7ab24
syang0624 <steven.yang@uni.minerva.edu>
2025-02-14 15:51:43 +0900

    problem 1679 done

---
+++ Medium/1679MaxNumberOfKSumPairs/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        d = defaultdict(int)
+        pairs = 0
+
+        for num in nums:
+            if d[k - num] > 0:
+                pairs += 1
+                d[k - num] -= 1
+            else:
+                d[num] += 1
+        return pairs
+    
+# Time Complexity: O(N)
+# Space Complexity: O(N)
