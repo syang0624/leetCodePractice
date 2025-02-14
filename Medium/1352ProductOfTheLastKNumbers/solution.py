253561e719fc5a35efa47b569387244f4b590bc0
syang0624 <steven.yang@uni.minerva.edu>
2025-02-14 14:41:11 +0900

    problem 1352 done

---
+++ Medium/1352ProductOfTheLastKNumbers/solution.py
@@ -0,0 +1,22 @@
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.prefix = [1]
+
+    def add(self, num: int) -> None:
+        if num != 0:
+            self.prefix.append(self.prefix[-1]*num)
+        else:
+            self.prefix=[1]
+
+    def getProduct(self, k: int) -> int:
+        self.size  = len(self.prefix) - 1
+        if k > self.size:
+            return 0
+        return self.prefix[self.size] // self.prefix[self.size - k]
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
