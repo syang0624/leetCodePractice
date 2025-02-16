5ae5b46b314b2cd821e1aa67261666fc3169125a
syang0624 <steven.yang@uni.minerva.edu>
2025-02-16 17:21:54 +0900

    problem 1207 clean code & complexity analysis update

--- Easy/1207UniqueNumberOfOccurrences/solution.py
+++ Easy/1207UniqueNumberOfOccurrences/solution.py
@@ -1,16 +1,14 @@
 from typing import List
 
-
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         count = {}
-        for i in range(len(arr)):
-            if arr[i] in count:
-                count[arr[i]] += 1
+        for a in arr:
+            if a in count:
+                count[a] += 1
             else:
-                count[arr[i]] = 1
+                count[a] = 1
+        return len(set(count.values())) == len(count)
 
-        if len(set(count)) == len(set(count.values())):
-            return True
-        else:
-            return False
+# Time Complexity: O(N)
+# Space Complexity: O(N)
