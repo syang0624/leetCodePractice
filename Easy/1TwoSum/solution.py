3e759f46ed7dde0e692dd7cf412de7515943290f
syang0624 <steven.yang@uni.minerva.edu>
2025-03-03 15:55:01 +0900

    problem 1 clean code update

--- Easy/1TwoSum/solution.py
+++ Easy/1TwoSum/solution.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+# class Solution:
     # Brute Force
     # Time O(N^2)
     # Space O(1)
@@ -11,14 +11,17 @@ class Solution:
     #             if nums[x] + nums[y] == target:
     #                 return [x, y]
 
-    # HashMap
+
+    
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
     # Time O(N)
     # Space O(N)
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
-        for x in range(len(nums)):
-            hashMap[nums[x]] = x
-        for i in range(len(nums)):
-            y = target - nums[i]
-            if y in hashMap and hashMap[y] != i:
-                return [i, hashMap[y]]
