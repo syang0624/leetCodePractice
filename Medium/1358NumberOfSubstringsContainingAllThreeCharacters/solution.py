fe0acfc40d43fab98374c551976ea913c77964ed
syang0624 <steven.yang@uni.minerva.edu>
2025-03-11 20:11:02 +0900

    problem 1358 done

---
+++ Medium/1358NumberOfSubstringsContainingAllThreeCharacters/solution.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        l = 0
+        res = 0
+        count = defaultdict(int)
+
+        for r in range(len(s)):
+            count[s[r]] += 1
+            while len(count) == 3:
+                res += (len(s) - r)
+                count[s[l]] -= 1
+                if count[s[l]] == 0:
+                    count.pop(s[l])
+                l += 1
+        return res
+    
+# Time Complexity: O(N)
+# Space Complexity: O(1)
