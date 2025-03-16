931248583669d29fa08e1a9aee01d3cee8d9d4f7
syang0624 <steven.yang@uni.minerva.edu>
2025-03-16 22:17:57 +0800

    problem 2594 done

---
+++ Medium/2594MinimumTimeToRepairCars/solution.py
@@ -0,0 +1,25 @@
+from typing import List
+from math import sqrt
+
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        def count_repaired(time):
+            count = 0
+            for r in ranks:
+                count += int(sqrt(time / r))
+            return count
+        l, r = 1, ranks[0] * cars * cars
+        res = -1
+        while l <= r:
+            m = (l + r) // 2
+            repaired = count_repaired(m)
+            if repaired >= cars:
+                res = m
+                r = m -1
+            else:
+                l = m + 1
+        return res
+    
+
+# Time Complexity: O(n log cars)
+# Space Complexity: O(1)
