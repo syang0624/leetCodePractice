5eb66189257d5b7a1c7fff3c96a4f399d60c92ca
syang0624 <steven.yang@uni.minerva.edu>
2025-03-18 15:58:23 +0800

    problem 2401 done

---
+++ Medium/2401LongestNiceSubarray/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        res = 0
+        cur = 0
+        l = 0
+        for r in range(len(nums)):
+            while cur & nums[r]:
+                cur = cur ^ nums[l]
+                l += 1
+            res = max(res, r - l + 1)
+            cur = cur ^ nums[r]
+        return res
+    
+# Time Complexity: O(N)
+# Space Complexity: O(1)
