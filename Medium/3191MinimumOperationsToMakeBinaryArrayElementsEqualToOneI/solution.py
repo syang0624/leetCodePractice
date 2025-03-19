494c0f6d97b9dd6e944c0564c3668f08cf1e8ce4
syang0624 <steven.yang@uni.minerva.edu>
2025-03-19 17:00:25 +0900

    problem 3191 done

---
+++ Medium/3191MinimumOperationsToMakeBinaryArrayElementsEqualToOneI/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        def flip(nums, i):
+            nums[i] = 0 if nums[i] else 1
+        
+        res = 0
+        for i in range(len(nums)- 2):
+            if nums[i] == 0:
+                flip(nums, i)
+                flip(nums, i+1)
+                flip(nums, i+2)
+                res += 1
+        if not nums[-1] or not nums[-2]:
+            return -1
+        return res
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
