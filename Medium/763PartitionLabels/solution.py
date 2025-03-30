d44275a35271cb5849cbab94bb71aebe2dcd2b91
syang0624 <steven.yang@uni.minerva.edu>
2025-03-30 18:59:57 +0900

    problem 763 done

---
+++ Medium/763PartitionLabels/solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIndex = {}
+        for i, c in enumerate(s):
+            lastIndex[c] = i
+        res = []
+        size, end = 0, 0
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[c])
+
+            if i == end:
+                res.append(size)
+                size = 0
+        
+        return res
+    
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
