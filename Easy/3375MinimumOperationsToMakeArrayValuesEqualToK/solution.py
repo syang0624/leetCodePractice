4458761e9045994c771c61986accc6f71dfa744f
syang0624 <steven.yang@uni.minerva.edu>
2025-04-09 19:48:51 +0900

    problem 3375 done

---
+++ Easy/3375MinimumOperationsToMakeArrayValuesEqualToK/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        st = set()
+        for x in nums:
+            if x < k:
+                return -1
+            elif x > k:
+                st.add(x)
+        return len(st)
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
