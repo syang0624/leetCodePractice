ebc8dfe7ac96224ea66d8d890e2ca02ae95e53b4
syang0624 <steven.yang@uni.minerva.edu>
2025-04-19 21:26:14 +0900

    problem 2563 done

---
+++ Medium/2563CountTheNumberOfFairPairs/solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        def binary_search(l, r, target):
+            while l <= r:
+                m = (l + r) // 2
+                if nums[m] >= target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            return r
+        
+        nums.sort()
+        res = 0
+        for i in range(len(nums)):
+            low = lower - nums[i]
+            up = upper - nums[i]
+            res += (
+                binary_search(i+1, len(nums)-1, up + 1) -
+                binary_search(i+1, len(nums)-1, low)
+            )
+            
+        return res
+    
+# Time Complexity: O(n log n)
+# Space Complexity: O(1)
