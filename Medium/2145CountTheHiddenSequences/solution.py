c9300ad6ef056dd020608bd3367c0b803b3a76aa
syang0624 <steven.yang@uni.minerva.edu>
2025-04-21 17:00:40 +0900

    problem 2145 done

---
+++ Medium/2145CountTheHiddenSequences/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        x = y = cur = 0
+        for d in differences:
+            cur += d
+            x = min(x, cur)
+            y = max(y, cur)
+            if y - x > upper - lower:
+                return 0
+        return (upper - lower) - (y - x) + 1
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
