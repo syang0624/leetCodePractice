1654595b94f562f261d70cd4f4ea996e5c531187
syang0624 <steven.yang@uni.minerva.edu>
2025-04-28 14:09:18 +0900

    problem 2302 done

---
+++ Hard/2302CountSubarraysWithScoreLessThanK/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        res, total = 0, 0
+        i = 0
+        for j in range(n):
+            total += nums[j]
+            while i <= j and total * (j - i + 1) >= k:
+                total -= nums[i]
+                i += 1
+            res += j - i + 1
+        return res
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
