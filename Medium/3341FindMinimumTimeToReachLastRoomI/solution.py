d68bdf800893f3ab435b952db18c181f9275b55c
syang0624 <steven.yang@uni.minerva.edu>
2025-05-07 15:36:09 +0900

    problem 3341 done

---
+++ Medium/3341FindMinimumTimeToReachLastRoomI/solution.py
@@ -0,0 +1,31 @@
+import heapq
+from typing import List
+
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        m, n = len(moveTime), len(moveTime[0])
+        dir = [-1, 0, 1, 0 , -1]
+        
+        def isValid(r, c, m, n):
+            return 0 <= r < m and 0 <= c < n
+        
+        minHeap = [(0, 0, 0)]
+        visited = [[False] * n for _ in range(m)]
+        visited[0][0] = True
+
+        while minHeap:
+            time, r, c = heapq.heappop(minHeap)
+            if r == m - 1 and c == n - 1:
+                return time
+            
+            for i in range(4):
+                newR, newC = r + dir[i], c + dir[i+1]
+                if isValid(newR, newC, m, n) and not visited[newR][newC]:
+                    newTime = 1 + max(time, moveTime[newR][newC])
+                    heapq.heappush(minHeap, (newTime, newR, newC))
+                    visited[newR][newC] = True
+        
+        return -1
+
+# Time Complexity: O(mn * log(mn))
+# Space Complexity: O(mn)
