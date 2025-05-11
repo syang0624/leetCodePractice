45e288cefa9f32ca300e2899d2373c731c76fe5d
syang0624 <steven.yang@uni.minerva.edu>
2025-05-11 15:48:56 +0900

    problem 169 done

---
+++ Easy/169MajorityElement/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res = majority = 0
+        
+        for n in nums:
+            if majority == 0:
+                res = n
+            
+            majority += 1 if n == res else -1
+        
+        return res
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
