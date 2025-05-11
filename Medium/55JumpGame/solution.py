3812f989443fd3576b34bbf9b0cbe57cfb347e81
syang0624 <steven.yang@uni.minerva.edu>
2025-05-11 17:24:56 +0900

    problem 55 done

---
+++ Medium/55JumpGame/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums) -1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return True if goal == 0 else False
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
