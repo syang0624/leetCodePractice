95ba3372f5d33878c19e8fea26b4bb271fa94683
syang0624 <steven.yang@uni.minerva.edu>
2025-05-15 11:28:22 +0900

    problem 2900 done

---
+++ Easy/2900LongestUnequalAdjacentGroupsSubsequenceI/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        return [words[0]] + [words[i] for i in range(1, len(groups)) if groups[i] != groups[i - 1]]
+    
+# Time Complexity: O(N)
+# Space Complexity: O(1)
