b66119e3e11226c3ce9c48a6072799e525d7a2cd
syang0624 <steven.yang@uni.minerva.edu>
2025-05-18 14:27:22 +0900

    problem 134 done

---
+++ Medium/134GasStation/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        current_gas = 0
+        start = 0
+        for i in range(len(gas)):
+            current_gas += gas[i] - cost[i]
+            if current_gas < 0:
+                current_gas = 0
+                start = i+1
+        return start
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
