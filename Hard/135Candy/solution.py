74c44f19d3e0a4a675d802b0c55d6f11884def93
syang0624 <steven.yang@uni.minerva.edu>
2025-05-18 14:42:29 +0900

    problem 135 done

---
+++ Hard/135Candy/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        result = [1] * len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                result[i] = result[i-1] + 1
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                if result[i] <= result[i+1]:
+                    result[i] = result[i+1] + 1
+        return sum(result)
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
