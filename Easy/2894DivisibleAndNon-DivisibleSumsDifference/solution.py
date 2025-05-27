b24038c0774f6e0e7b81ecee5b6e122df1f3641c
syang0624 <steven.yang@uni.minerva.edu>
2025-05-27 20:46:11 +0900

    problem 2894 done

---
+++ Easy/2894DivisibleAndNon-DivisibleSumsDifference/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = 0
+        num2 = 0
+        for i in range(1, n+1):
+            if i % m != 0:
+                num1 += i
+            else:
+                num2 += i
+        return num1 - num2
+    
+# Time Complexity: O(N)
+# Space Complexity: O(1)
