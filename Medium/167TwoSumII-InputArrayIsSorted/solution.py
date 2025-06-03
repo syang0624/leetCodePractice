39314229fba6537f2e2adaf12f8c90f9f4e90473
syang0624 <steven.yang@uni.minerva.edu>
2025-06-03 22:54:43 +0900

    problem 167 done

---
+++ Medium/167TwoSumII-InputArrayIsSorted/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            total = numbers[left] + numbers[right]
+
+            if total == target:
+                return [left + 1, right + 1]
+            elif total > target:
+                right -= 1
+            else:
+                left += 1
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
