299fa27dab76959232b48e79cfd24e3d3256c78e
syang0624 <steven.yang@uni.minerva.edu>
2025-06-08 17:38:40 +0900

    problem 386 done

---
+++ Medium/386LexicographicalNumbers/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        
+        def dfs(cur):
+            if cur > n:
+                return
+            res.append(cur)
+            cur *= 10
+            for i in range(10):
+                dfs(cur + i)
+            
+        
+        for i in range(1, 10):
+            dfs(i)
+
+        return res
+    
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
