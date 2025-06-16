ee0c43ca03f098a8341fc78d67404578506148cb
syang0624 <steven.yang@uni.minerva.edu>
2025-06-16 19:54:06 +0900

    problem 2016 done

---
+++ Easy/2016MaximumDifferenceBetweenIncreasingElements/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans, premin = -1, nums[0]
+
+        for i in range(1, n):
+            if nums[i] > premin:
+                ans = max(ans, nums[i] - premin)
+            else:
+                premin = nums[i]
+
+        return ans
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
