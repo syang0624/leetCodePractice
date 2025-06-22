025ce9485d65a386743471506f702fa0691b1129
syang0624 <steven.yang@uni.minerva.edu>
2025-06-22 18:42:58 +0900

    problem 2138 done

---
+++ Easy/2138DivideAStringIntoGroupsOfSizeK/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        res = []  # grouped string
+        n = len(s)
+        curr = 0  # starting index of each group
+        # split string
+        while curr < n:
+            res.append(s[curr : curr + k])
+            curr += k
+        # try to fill in the last group
+        res[-1] += fill * (k - len(res[-1]))
+        return res
+
+# Time Complexity: O(max(n, k))
+# Space Complexity: O(1)
