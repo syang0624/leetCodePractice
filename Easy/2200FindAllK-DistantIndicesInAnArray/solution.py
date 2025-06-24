6b9e65831190813a527053a7ac83100bf37fdb62
syang0624 <steven.yang@uni.minerva.edu>
2025-06-24 17:38:17 +0900

    problem 2200 done

---
+++ Easy/2200FindAllK-DistantIndicesInAnArray/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        res = []
+        n = len(nums)
+        # traverse number pairs
+        for i in range(n):
+            for j in range(n):
+                if nums[j] == key and abs(i - j) <= k:
+                    res.append(i)
+                    break  # early termination to prevent duplicate addition
+        return res
+
+# Time Complexity: O(N^2)
+# Space Complexity: O(1)
