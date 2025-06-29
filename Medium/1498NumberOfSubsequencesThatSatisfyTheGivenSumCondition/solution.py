6b151621b4e35d1fb795e30ea1deba76bcf817e6
syang0624 <steven.yang@uni.minerva.edu>
2025-06-29 22:10:22 +0900

    problem 1498 done

---
+++ Medium/1498NumberOfSubsequencesThatSatisfyTheGivenSumCondition/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        res = 0
+        mod = (10**9 + 7)
+
+        r = len(nums) - 1
+        for i, left in enumerate(nums):
+            while (left + nums[r]) > target and i <= r:
+                r -= 1
+            if i <= r:
+                res += (2**(r - i))
+                res %= mod
+        
+        return res
+
+# Time Complexity: O(N log N)
+# Space Complexity: O(1)
