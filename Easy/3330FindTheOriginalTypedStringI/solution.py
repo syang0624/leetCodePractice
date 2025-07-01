69d4d6e0552cc005b0ec7f30affbf5df04a20b39
syang0624 <steven.yang@uni.minerva.edu>
2025-07-01 21:26:31 +0900

    problem 3330 done

---
+++ Easy/3330FindTheOriginalTypedStringI/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        n, ans = len(word), 1
+        for i in range(1, n):
+            if word[i - 1] == word[i]:
+                ans += 1
+        return ans
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
