01035382f877738150868707ff034fe0e88ce41a
syang0624 <steven.yang@uni.minerva.edu>
2025-07-09 22:59:26 +0900

    update solution for problem 242

--- Easy/242ValidAnagram/solution.py
+++ Easy/242ValidAnagram/solution.py
@@ -1,8 +1,10 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = sorted(list(s))
-        t = sorted(list(t))
-        if s == t:
-            return True
-        else:
-            return False
+        # s = sorted(list(s))
+        # t = sorted(list(t))
+        # if s == t:
+        #     return True
+        # else:
+        #     return False
+        return Counter(s) == Counter(t)
