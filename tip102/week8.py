abbb652a4f2543cc69183132b038cfe966068eee
syang0624 <steven.yang@uni.minerva.edu>
2025-07-25 07:21:40 +0530

    codepath: week 8 session 2

--- tip102/week8.py
+++ tip102/week8.py
@@ -1,81 +1,232 @@
-# class TreeNode:
-#     def __init__(self, value, left=None, right=None):
-#         self.val = value
-#         self.left = left
-#         self.right = right
+from collections import deque 
 
-# def right_vine(root):
-#     if not root:
-#         return []
-#     arr = [root.val]
-#     while root.right:
-#         arr.append(root.right.val)
-#         root = root.right
-#     return arr
+# Tree Node class
+def build_tree(va
+               lues):
+  if not values:
+      return None
+
+  def get_key_value(item):
+      if isinstance(item, tuple):
+          return item[0], item[1]
+      else:
+          return None, item
+
+  key, value = get_key_value(values[0])
+  root = TreeNode(value, key)
+  queue = deque([root])
+  index = 1
+
+  while queue:
+      node = queue.popleft()
+      if index < len(values) and values[index] is not None:
+          left_key, left_value = get_key_value(values[index])
+          node.left = TreeNode(left_value, left_key)
+          queue.append(node.left)
+      index += 1
+      if index < len(values) and values[index] is not None:
+          right_key, right_value = get_key_value(values[index])
+          node.right = TreeNode(right_value, right_key)
+          queue.append(node.right)
+      index += 1
+
+  return root
+
+
+
+class TreeNode:
+    def __init__(self, val, key, left=None, right=None):
+        self.key = key      # Plant price
+        self.val = val      # Plant name
+        self.left = left
+        self.right = right
+
+
+def sort_plants(collection):
+    plant_list = []
+    def inorder(node):
+        if not node:
+            return
+        # left , add to list, right child
+        inorder(node.left)
+        plant_list.append((node.key,node.val))
+        inorder(node.right)
+    inorder(collection)
+    return plant_list
+
+"""
+         (3, "Monstera")
+        /               \
+   (1, "Pothos")     (5, "Witchcraft Orchid")
+        \                 /
+  (2, "Spider Plant")   (4, "Hoya Motoskei")
+"""
+
+# Using build_tree() function at the top of page
+values = [(3, "Monstera"), (1, "Pothos"), (5, "Witchcraft Orchid"), None, (2, "Spider Plant"), (4, "Hoya Motoskei")]
+collection = build_tree(values)
+
+print(sort_plants(collection))
+
+
+from collections import deque
+class TreeNode():
+     def __init__(self, value, left=None, right=None):
+         self.val = value
+         self.left = left
+         self.right = right
+         
+def find_flower(inventory, name):
+    
+    while(inventory is not None):
+        # check if the value = name 
+        # if yes, return true
+        curr = inventory.val.lower()
+        if (curr == name.lower()):
+            return True 
+        # if inventory is less alphebetacilly then name
+        # right
+        elif (curr < name.lower()):
+            inventory = inventory.right
+        
+        # if inventory is more alphabetically
+        # left 
+        else:
+            inventory = inventory.left
+    return False
+    
+   
+
+
+
+    
+def build_tree(values):
+  if not values:
+      return None
+
+  def get_key_value(item):
+      if isinstance(item, tuple):
+          return item[0], item[1]
+      else:
+          return None, item
+
+  key, value = get_key_value(values[0])
+  root = TreeNode(value, key)
+  queue = deque([root])
+  index = 1
+
+  while queue:
+      node = queue.popleft()
+      if index < len(values) and values[index] is not None:
+          left_key, left_value = get_key_value(values[index])
+          node.left = TreeNode(left_value, left_key)
+          queue.append(node.left)
+      index += 1
+      if index < len(values) and values[index] is not None:
+          right_key, right_value = get_key_value(values[index])
+          node.right = TreeNode(right_value, right_key)
+          queue.append(node.right)
+      index += 1
+
+  return root
+
 # """
-#         Root
-#       /      \
-#     Node1    Node2
-#   /         /    \
-# Leaf1    Leaf2  Leaf3
+#         Rose
+#         /    \
+#      Lilac   Tulip
+#      /  \       \
+#   Daisy  Lily   Violet
 # """
-# ivy1 = TreeNode("Root", 
-#                 TreeNode("Node1", TreeNode("Leaf1")),
-#                 TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
 
-# """
-#       Root
-#       /  
-#     Node1
-#     /
-#   Leaf1  
-# """
-# ivy2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
+# using build_tree() function at top of page
+values = ["Rose", "Lilac", "Tulip", "Daisy", "Lily", None, "Violet"]
+garden = build_tree(values)
 
-# print(right_vine(ivy1))
-# print(right_vine(ivy2))
+print(find_flower(garden, "Lilac"))  
+print(find_flower(garden, "Sunflower")) 
+
+from collections import deque
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def add_plant(collection, name):
+
+    if collection is None:
+        return TreeNode(name)
+    
+    if collection.val > name:
+        collection.left = add_plant(collection.left, name)
+    else:
+        collection.right = add_plant(collection.right, name)
+    return collection 
+   
+
+def build_tree(values):
+  if not values:
+      return None
+
+  def get_key_value(item):
+      if isinstance(item, tuple):
+          return item[0], item[1]
+      else:
+          return None, item
+
+  key, value = get_key_value(values[0])
+  root = TreeNode(value, key)
+  queue = deque([root])
+  index = 1
+
+  while queue:
+      node = queue.popleft()
+      if index < len(values) and values[index] is not None:
+          left_key, left_value = get_key_value(values[index])
+          node.left = TreeNode(left_value, left_key)
+          queue.append(node.left)
+      index += 1
+      if index < len(values) and values[index] is not None:
+          right_key, right_value = get_key_value(values[index])
+          node.right = TreeNode(right_value, right_key)
+          queue.append(node.right)
+      index += 1
+
+  return root
 
 
-# class TreeNode:
-#     def __init__(self, value, left=None, right=None):
-#         self.val = value
-#         self.left = left
-#         self.right = right
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
 
-# def right_vine(root):
-#         def helper(node):
-             
-#                 return node.right.val
-#         if not root:
-#             return []
-#         arr = [root.val]
-#         while root.right:
-#             arr.append(root.right.val)
-#             root = root.right
-#         return arr
+#             Money Tree
+#         /              \
+# Fiddle Leaf Fig    Snake Plant
+# /
+# Aloe 
 
-# """
-#         Root
-#       /      \
-#     Node1    Node2
-#   /         /    \
-# Leaf1    Leaf2  Leaf3
-# """
-# ivy1 = TreeNode("Root", 
-#                 TreeNode("Node1", TreeNode("Leaf1")),
-#                 TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
+# Using build_tree() function at the top of page
+values = ["Money Tree", "Fiddle Leaf Fig", "Snake Plant"]
+collection = build_tree(values)
 
-# """
-#       Root
-#       /  
-#     Node1
-#     /
-#   Leaf1  
-# """
-# ivy2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
+# Using print_tree() function at the top of page
+print_tree(add_plant(collection, "Aloe"))
 
-# print(right_vine(ivy1))
-# print(right_vine(ivy2))
+
+from collections import deque 
 
 class TreeNode:
     def __init__(self, value, left=None, right=None):
@@ -83,30 +234,87 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def survey_tree(root):
-    res = []
-    # Something magical here to do a postorder traversal!
-    def helper(node):
-        if not node:
-            return
-        helper(node.left)
-        helper(node.right)
-        res.append(node.val)
-    helper(root)
+def remove_plant(collection, name):
+    if collection.val is name:
+        # no children 
+        if (collection.left is None and collection.right is None):
+            collection = None
+            return 
+        
+        # run three test cases here
+        if collection.left:
+        
+    
+    if collection.val > name:
+        remove_plant(collection.left, name)
+    else:
+        remove_plant(collection.right, name)
+    return collection 
 
-    return res
+
+def print_tree(root):
+    if not root:
+        return "Empty"
+    result = []
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    print(result)
 
 
-"""
-        Root
-      /      \
-    Node1    Node2
-  /         /    \
-Leaf1    Leaf2  Leaf3
-"""
+def build_tree(values):
+  if not values:
+      return None
 
-magnolia = TreeNode("Root", 
-                TreeNode("Node1", TreeNode("Leaf1")),
-                        TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
+  def get_key_value(item):
+      if isinstance(item, tuple):
+          return item[0], item[1]
+      else:
+          return None, item
 
-print(survey_tree(magnolia))
+  key, value = get_key_value(values[0])
+  root = TreeNode(value, key)
+  queue = deque([root])
+  index = 1
+
+  while queue:
+      node = queue.popleft()
+      if index < len(values) and values[index] is not None:
+          left_key, left_value = get_key_value(values[index])
+          node.left = TreeNode(left_value, left_key)
+          queue.append(node.left)
+      index += 1
+      if index < len(values) and values[index] is not None:
+          right_key, right_value = get_key_value(values[index])
+          node.right = TreeNode(right_value, right_key)
+          queue.append(node.right)
+      index += 1
+
+  return root
+
+
+
+
+
+# """
+#               Money Tree
+#              /         \
+#            Hoya        Pilea
+#               \        /   \
+#              Ivy    Orchid  ZZ Plant
+# """
+
+# Using build_tree() function at the top of page
+values = ["Money Tree", "Hoya", "Pilea", None, "Ivy", "Orchid", "ZZ Plant"]
+collection = build_tree(values)
+
+# Using print_tree() function at the top of page
+print_tree(remove_plant(collection, "Pilea"))
