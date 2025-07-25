2f3b24437a153952e9bbaf7eeab71a2c938f211f
syang0624 <steven.yang@uni.minerva.edu>
2025-07-25 21:40:29 +0530

    problem 103 done

---
+++ Medium/103BinaryTreeZigzagLevelOrderTraversal/solution.py
@@ -0,0 +1,28 @@
+from typing import Optional, List
+from collections import deque
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = deque([root] if root else [])
+        while q:
+            n = len(q)
+            level = []
+            for i in range(n):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level = list(reversed(level)) if len(res) % 2 else level
+            res.append(level)
+        return res
+
+# Time Complexity: O(N)
+# Space Complexity: O(N/2)
