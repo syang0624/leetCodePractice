025999a79f4e1bcd80de23da65c3cff4ea74e860
syang0624 <steven.yang@uni.minerva.edu>
2025-07-30 17:12:53 +0530

    problem 2419 done

---
+++ Medium/2419LongestSubarrayWithMaximumBitwiseAND/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_val = ans = current_streak = 0
+        for num in nums:
+            if max_val < num:
+                max_val = num
+                ans = current_streak = 0
+
+            if max_val == num:
+                current_streak += 1
+            else:
+                current_streak = 0
+
+            ans = max(ans, current_streak)
+        return ans
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
