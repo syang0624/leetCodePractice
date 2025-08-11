ffe17caa94a6e7c5415ba88546ab3d48d0c44808
syang0624 <steven.yang@uni.minerva.edu>
2025-08-11 17:34:57 +0900

    problem 2438 done

---
+++ Medium/2438RangeProductQueriesOfPowers/solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        mod = 10**9 + 7
+        bins, rep = [], 1
+        while n > 0:
+            if n % 2 == 1:
+                bins.append(rep)
+            n //= 2
+            rep *= 2
+        ans = []
+        for left, right in queries:
+            cur = 1
+            for i in range(left, right + 1):
+                cur = cur * bins[i] % mod
+            ans.append(cur)
+        return ans
+
+# Time Complexity: O(n * log n)
+# Space Complexity: O(log n)
