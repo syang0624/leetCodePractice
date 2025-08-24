22a0205a9df5941bdae5b908bb2e61ddd1d1ca81
syang0624 <steven.yang@uni.minerva.edu>
2025-08-24 20:05:28 +0900

    problem 1493 done

---
+++ Medium/1493LongestSubarrayOf1'sAfterDeletingOneElement/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_len = 0
+        start = 0
+        zero = -1
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                start = zero + 1
+                zero = i
+            max_len = max(max_len, i - start)
+        
+        return max_len
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
