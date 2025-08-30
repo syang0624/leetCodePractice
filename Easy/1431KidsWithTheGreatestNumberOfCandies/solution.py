d0ec82ff2e61e4818763c762e4c8507ef58baa28
syang0624 <steven.yang@uni.minerva.edu>
2025-08-30 17:00:31 +0900

    problem 1431 solution update

--- Easy/1431KidsWithTheGreatestNumberOfCandies/solution.py
+++ Easy/1431KidsWithTheGreatestNumberOfCandies/solution.py
@@ -9,6 +9,17 @@ class Solution:
             else:
                 candies[i] = True
         return candies
+    
+
+# class Solution:
+#     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+#         maxCandies = max(candies)
+#         for i in range(len(candies)):
+#             if candies[i] + extraCandies >= maxCandies:
+#                 candies[i] = True
+#             else:
+#                 candies[i] = False
+#         return candies
 
 # Time Complexity: O(N)
 # Space Complexity: O(1)
