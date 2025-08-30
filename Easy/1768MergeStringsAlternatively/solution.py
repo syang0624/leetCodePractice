6135b76ed2283869ee42e721ad7d65defb9517f6
syang0624 <steven.yang@uni.minerva.edu>
2025-08-30 16:21:24 +0900

    problem 1768 updated solution

--- Easy/1768MergeStringsAlternatively/solution.py
+++ Easy/1768MergeStringsAlternatively/solution.py
@@ -11,3 +11,16 @@ class Solution:
         merged.append(word2[i:])
 
         return "".join(merged)
+
+# class Solution:
+#     def mergeAlternately(self, word1: str, word2: str) -> str:
+#         res = ""
+#         while word1 and word2:
+#             if word1:
+#                 res = res + word1[0]
+#                 word1 = word1[1:]
+#             if word2:
+#                 res = res + word2[0]
+#                 word2 = word2[1:]
+#         res = res + word1 + word2
+#         return res
